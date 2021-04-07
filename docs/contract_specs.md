6ac74eecd201c3927f7cc474277f4fc721b08f21
Cao Yizhou <yizhou.cao@taustrat.com>
2021-04-07 15:44:03 +0800

    add spec for hash rate futures

--- docs/contract_specs.md
+++
@@ -1,7 +0,0 @@
----
-id: contract_specs
-title: Contract Specs
----
-
-
-![img](../static/img/specs.png)
--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   docs: {
     "FAQ": ["general", "traders", "lp", "lp_and_maintainer", "advanced"],
-    "Contract Specs": ["contract_specs"],
+    "Contract Specs": ["asset_futures", "hash_rate_futures"],
     "Key Parameters": ["parameters"]
   },
 };
