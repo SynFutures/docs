e89eb6206eab83e42e3d27b5d26aea6f1650f714
Cao Yizhou <yizhou.cao@taustrat.com>
2021-04-13 14:05:39 +0800

    add tutorial

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,7 @@ module.exports = {
   docs: {
     "FAQ": ["general", "traders", "lp", "lp_and_maintainer", "advanced"],
     "Contract Specs": ["asset_futures", "hash_rate_futures"],
-    "Key Parameters": ["parameters"]
+    "Key Parameters": ["parameters"],
+    "Tutorials": ["tutorial_new_asset_pair"]
   },
 };
